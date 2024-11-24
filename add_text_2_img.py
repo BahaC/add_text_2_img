c57e7e6ccccadecb5c528bfd42c4cdaa966e0166
Bahadır Çalışkan <75085482+BahaC@users.noreply.github.com>
2024-11-24 20:43:59 +0300

    Updated add_text_2_img.py
    Opacity and rotation of the text can now be changed in ComfyUI,
    comment lines changed to english

--- add_text_2_img.py
+++ add_text_2_img.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class AddText:
     """
-    在图像上添加文字的节点
+    Node for adding text to an image
     """
 
     def __init__(self):
@@ -14,13 +14,13 @@ class AddText:
 
     @classmethod
     def INPUT_TYPES(s):
-        # 获取字体目录和所有字体文件
+        # Get the font directory and all font files
         script_dir = os.path.dirname(os.path.abspath(__file__))
         font_dir = os.path.join(script_dir, "fonts")
         available_fonts = ["Custom"] + \
                           [f[:-4] for f in os.listdir(font_dir) if
                            f.endswith(".ttf") | f.endswith(".TTF") | f.endswith(".ttc")]
-        available_fonts = list(set(available_fonts))  # 去重
+        available_fonts = list(set(available_fonts))  # Deduplication
 
         return {
             "required": {
@@ -34,6 +34,10 @@ class AddText:
                 "font_shadow_x": ("INT", {"default": 0, "min": 0, "max": 20, "step": 1, "display": "number"}),
                 "font_shadow_y": ("INT", {"default": 0, "min": 0, "max": 20, "step": 1, "display": "number"}),
                 "shadow_color": ("STRING", {"multiline": False, "default": "#000000"}),
+                "opacity": ("FLOAT", {"default": 1.0, "min": 0, "max": 1.0, "step": 0.1, "display": "number"}),
+                "rotation": ("INT", {"default": 0, "min": 0, "max": 360, "step": 5, "display": "number"}),
+
+
             },
             "optional": {
                 "custom_font_path": ("STRING", {"multiline": False, "default": "",
@@ -46,30 +50,30 @@ class AddText:
     CATEGORY = "AI_Boy"
 
     def add_text(self, image, text, x, y, font_size, font_family, font_color, font_shadow_x, font_shadow_y,
-                 shadow_color, custom_font_path=None):
+                 shadow_color, opacity, rotation, custom_font_path=None):
         """
-        在图像上添加文字
+        Add text to images
         """
 
-        # 获取原始图像的维度信息
+        # Get the dimension information of the original image
         orig_shape = image.shape
 
-        # 调整维度顺序
+        # Adjust the order of dimensions
         image = image.permute(0, 3, 1, 2)
 
-        # 设置字体
+        # Setting the font
         script_dir = os.path.dirname(os.path.abspath(__file__))
         font_dir = os.path.join(script_dir, "fonts")
 
-        # 解析字体路径
-        font_path = parse_font_path(font_family, custom_font_path, font_dir);
+        # Resolving font paths
+        font_path = parse_font_path(font_family, custom_font_path, font_dir)
         font = ImageFont.truetype(font_path, font_size)
 
-        # 解析颜色值
-        font_color = parse_font_color(font_color);
-        shadow_color = parse_font_color(shadow_color);
+        # Parsing color values
+        font_color = parse_font_color(font_color)
+        shadow_color = parse_font_color(shadow_color)
 
-        # 将 Tensor 转换为 PIL Image
+        # Convert Tensor to PIL Image
         images = []
         for i in range(image.shape[0]):
             img = torch.clamp(image[i], min=0., max=1.)
@@ -79,38 +83,51 @@ class AddText:
 
         processed_images = []
         for img in images:
-            # 获取图像的宽度和高度
+            # Get the width and height of an image
             image_width, image_height = img.size
-            # 检查 x, y 是否超出范围，如果超出则限制在图像范围内
+            # Check if x, y is out of range, if so clamp it to the image range
             x = max(0, min(x, image_width - 1))
             y = max(0, min(y, image_height - 1))
 
-            # 获取文字的宽度和高度
+            # Get the width and height of the text
             text_width, text_height = font.getmask(text).size
 
-            # 如果 x, y 没有设置，则默认文字在图片正下方居中
+            # If x, y are not set, the default text is centered just below the image.
             if x == 0 and y == 0:
-                # 计算文字的 x 坐标，使其水平居中
+                # Calculate the x coordinate of the text to center it horizontally
                 x = (image_width - text_width) // 2
-                # 计算文字的 y 坐标，使其位于图像正下方
-                y = image_height - 50  # 添加 50 个像素的间距
+                # Calculate the y coordinate of the text so that it is directly below the image
+                y = image_height - 50  # Add 50 pixels of spacing
 
-            # 创建绘图对象
-            draw = ImageDraw.Draw(img)
+            # Creating a drawing object
+            background = Image.new("RGBA", (image_width, image_height), (255, 255, 255, 0))
 
-            # 添加文字阴影
+            draw = ImageDraw.Draw(background)
+            
+            alpha = opacity * 255
+            alpha = int(alpha)
+            rgba_color = (*font_color, alpha)
+
+            background_2 = Image.new("RGBA", (text_width, text_height + font_shadow_y + 30), (255, 255, 255, 0))
+            draw_2 = ImageDraw.Draw(background_2)
+
             if font_shadow_x > 0 and font_shadow_y > 0:
-                draw.text((x + font_shadow_x, y + font_shadow_y), text, font=font, fill=shadow_color)
-            # 添加文字
-            draw.text((x, y), text, font=font, fill=font_color)
+                draw_2.text((font_shadow_x, font_shadow_y), text, font=font, fill=shadow_color)
+            # Add text
+            draw_2.text((0, 0), text, font=font, fill=rgba_color)
+
+            background_2 = background_2.rotate(rotation, expand=True)            
+
+            background.paste(background_2, (x,y))
+            img = Image.alpha_composite(img.convert("RGBA"), background).convert("RGB")
 
             processed_images.append(img)
 
-        # 将 PIL Image 转换回 Tensor
+        # Convert PIL Image back to Tensor
         processed_images = [np.array(img).astype(np.float32) / 255.0 for img in processed_images]
         image = torch.from_numpy(np.stack(processed_images, axis=0))
 
-        # 恢复原始图像的维度顺序
+        # Restore the original image dimension order
         image = image.reshape(orig_shape)
         return (image,)
 
@@ -124,40 +141,40 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 
-# 解析颜色值
+# Parsing color values
 def parse_font_color(font_color):
     if font_color.startswith("#"):
         font_color = font_color.lstrip('#')
         try:
             font_color = tuple(int(font_color[i:i + 2], 16) for i in (0, 2, 4))
         except ValueError:
-            raise ValueError("无效的十六进制颜色值: {}".format(font_color))
+            raise ValueError("Invalid hexadecimal color value: {}".format(font_color))
     else:
         try:
             font_color = tuple(map(int, font_color.split(',')))
             if len(font_color) != 3:
-                raise ValueError("RGB 颜色值必须包含三个整数")
+                raise ValueError("RGB color values ​​must contain three integers")
         except ValueError:
-            raise ValueError("无效的 RGB 颜色值: {}".format(font_color))
+            raise ValueError("Invalid RGB color value: {}".format(font_color))
     return font_color
 
 
-# 解析字体路径
+# Resolving font paths
 def parse_font_path(font_family, custom_font_path, font_dir):
     if font_family == "Custom":
         if not os.path.exists(custom_font_path):
-            raise ValueError(f"自定义字体路径不存在: {custom_font_path}")
+            raise ValueError(f"The custom font path does not exist: {custom_font_path}")
         font_path = custom_font_path
     else:
         font_path = os.path.join(font_dir, f"{font_family}.ttf")
         if not os.path.exists(font_path):
             font_path = os.path.join(font_dir, f"{font_family}.TTF")
-        # 如果 .ttf 文件不存在，则尝试 .ttc 文件
+        # If the .ttf file does not exist, the .ttc file is tried
         if not os.path.exists(font_path):
             font_path = os.path.join(font_dir, f"{font_family}.ttc")
         if not os.path.exists(font_path):
             font_path = os.path.join(font_dir, f"{font_family}.TTC")
-    # 检查字体文件是否存在
+    # Check if the font file exists
     if not os.path.exists(font_path):
-        raise FileNotFoundError(f"字体文件未找到: {font_path}")
+        raise FileNotFoundError(f"Font file not found: {font_path}")
     return font_path
